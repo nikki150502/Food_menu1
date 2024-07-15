1c317da616d997b9c40e70de250d2c285668eb5a
unknown <nikitakumawat690@gmail.com>
2024-07-15 07:46:08 +0530

    food

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,305 @@
+ 
+
+const Menu = [
+
+    {
+id: 1,
+image: "images/maggi.jpg ",
+name:  "Maggi",
+category: "Breakfast",
+price:  " Rs 20/",
+description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+},
+
+    
+    {
+id: 2,
+image: "images/coffee.jpg ",
+name:  "Coffee",
+category: "Breakfast",
+price: "Rs 20/",
+description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+},
+
+    
+{
+    id: 3,
+    image: "images/cold coffee.jpg ",
+    name:  "Cold coffee",
+    category: "Lunch",
+    price: "Rs 80/",
+    description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+    },
+
+
+        
+    {
+        id: 4,
+        image: "images/dal bati.jpg ",
+        name:  "Dal bati",
+        category: "Dinner",
+        price: "Rs 100/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 5,
+        image: "images/icecream.jpg ",
+        name:  "Ice-cream",
+        category: "Dinner",
+        price: "Rs 50/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 6,
+        image: "images/idli sambhr.jpg ",
+        name:  "Idli sambhar",
+        category: "Breakfast",
+        price: "Rs 60/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 7,
+        image: "images/momos.jpg ",
+        name:  "Momos",
+        category: "Evening",
+        price: "Rs 60/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 8,
+        image: "images/noodels.png ",
+        name:  "Noodles",
+        category: "Evening",
+        price: "Rs 80/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 9,
+        image: "images/paneer.jpg ",
+        name:  "Paneer",
+        category: "Dinner",
+        price: "Rs 150/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 10,
+        image: "images/panner masala.jpg ",
+        name:  "Panner masala",
+        category: "Dinner",
+        price: "Rs 200/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 11,
+        image: "images/chhola bhatura.jpg ",
+        name:  "Chhola bhatura",
+        category: "Breakfast",
+        price: "Rs 60/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+            
+    {
+        id: 12,
+        image: "images/paratha.jpg ",
+        name:  "Paratha",
+        category: "Breakfast",
+        price: "Rs 20/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 13,
+        image: "images/pasta.png ",
+        name:  "Pasta",
+        category: "Breakfast",
+        price: "Rs 40/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+
+            
+    {
+        id: 14,
+        image: "images/Pizza.jpg ",
+        name:  "Pizza",
+        category: "Evening",
+        price: "Rs 220/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+                
+    {
+        id: 15,
+        image: "images/raita.jpg ",
+        name:  "Raita",
+        category: "Lunch",
+        price: "Rs 20/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+                
+    {
+        id: 16,
+        image: "images/samosa.jpg ",
+        name:  " samosa",
+        category: "Evening",
+        price: "Rs 20/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+                
+    {
+        id: 17,
+        image: "images/steam momos.jpg ",
+        name:  "steam momos",
+        category: "Evening",
+        price: "Rs 40/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+                
+    {
+        id: 18,
+        image: "images/sweet.jpg ",
+        name:  "sweet",
+        category: "Dinner",
+        price: "Rs 200/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+                
+    {
+        id: 19,
+        image: "images/vada pav.jpg ",
+        name:  " vada pav",
+        category: "Evening",
+        price: "Rs 20/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+                
+    {
+        id: 20,
+        image: "images/vegthali.png ",
+        name:  " vegthali",
+        category: "Dinner",
+        price: "Rs 200/",
+        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+        },
+
+        {
+            id: 21,
+            image: "images/french fries.jpg ",
+            name:  "French fries",
+            category: "Evening",
+            price: "Rs 60/",
+            description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+            },
+
+            {
+                id: 22,
+                image: "images/juice.jpg ",
+                name:  "Juice",
+                category: "Breakfast",
+                price: "Rs 30/",
+                description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                },
+
+                {
+                    id: 23,
+                    image: "images/niku special.png ",
+                    name:  "Niku's special",
+                    category: "Dinner",
+                    price: "",
+                    description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                    },
+
+                    {
+                        id: 24,
+                        image: "images/pav bhaji.jpg ",
+                        name:  "Pav bhaji",
+                        category: "Breakfast",
+                        price: "Rs 70/",
+                        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                        },
+
+                        {
+                            id: 25,
+                            image: "images/ras malai.jpg ",
+                            name:  "Ras Malai",
+                            category: "Dinner",
+                            price: "Rs 120/",
+                            description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                            },
+
+                            
+                        {
+                            id: 26,
+                            image: "images/tea.jpg.jpg",
+                            name: "Tea",
+                            category: "Breakfast",
+                            price: "Rs 10/",
+                            description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                            },
+
+                            {
+                                id: 27,
+                                image: "images/upma.jpg",
+                                name:  "Upma",
+                                category: "Breakfast",
+                                price: "Rs 20/",
+                                description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                                },
+
+                                {
+                                    id: 29,
+                                    image: "images/poha.jpg.jpg",
+                                    name:  "Poha",
+                                    category: "Breakfast",
+                                    price: "Rs 20/",
+                                    description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                                    },
+
+                                    {
+                                        id: 30,
+                                        image: "images/sev.gpg.jpg",
+                                        name:  "Sev",
+                                        category: "Lunch",
+                                        price: "Rs 80/",
+                                        description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                                        },
+
+                                        {
+                                            id: 31,
+                                            image: "images/thali.jpg",
+                                            name:  "Thali",
+                                            category: "Lunch",
+                                            price: "Rs 100/",
+                                            description: " A cup of Maggi, a movie queued up—college life personified. Maggi is not only delightful but also delectable.",
+                                            },
+                                     
+
+];
+export default Menu;
